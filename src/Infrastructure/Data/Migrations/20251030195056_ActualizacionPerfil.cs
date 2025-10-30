99e40c4f57405a3b738ef4d4991c9331eee63d2e
kdevmilk <69746299+Belacqua1@users.noreply.github.com>
2025-10-30 17:05:40 -0300

    feat(user): add PendingEmail, VerificationCode, and VerificationCodeExpires columns to AspNetUsers table

---
+++ src/Infrastructure/Data/Migrations/20251030195056_ActualizacionPerfil.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiendaUCN.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizacionPerfil : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PendingEmail",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationCode",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerificationCodeExpires",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PendingEmail",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCodeExpires",
+                table: "AspNetUsers");
+        }
+    }
+}
