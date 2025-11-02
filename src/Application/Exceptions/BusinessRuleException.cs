b69340ecdf3074335c428365015b89d37ad377b2
Jhoanck7 <jhoan.montero@alumnos.ucn.cl>
2025-11-02 17:39:19 -0300

    feat(exceptions): add custom exception classes for business rules, conflicts, and not found scenarios

---
+++ src/Application/Exceptions/BusinessRuleException.cs
@@ -0,0 +1,8 @@
+namespace TiendaUCN.src.Application.Exceptions
+{
+    public class BusinessRuleException : Exception
+    {
+        public BusinessRuleException(string message)
+            : base(message) { }
+    }
+}
---
+++ src/Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+namespace TiendaUCN.src.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
