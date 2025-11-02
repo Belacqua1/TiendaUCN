954288bd894ea6c6f5b7c243303061df7f9d5609
Jhoanck7 <jhoan.montero@alumnos.ucn.cl>
2025-11-02 17:36:56 -0300

    feat(models): add Description, Slug, and IsDeleted properties to Brand and Category classes

--- src/Domain/Models/Category.cs
+++ src/Domain/Models/Category.cs
@@ -4,6 +4,9 @@ namespace TiendaUCN.src.Domain.Models
     {
         public int Id { get; set; }
         public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string Slug { get; set; } = null!;
+        public bool IsDeleted { get; set; } = false;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
